2d14c231a8466172ae5cc0c7b029eadb5f744013
Lucas alves <lucasalvesdarocha007@gmail.com>
2023-11-23 16:28:48 -0300

    ultimo commit

--- javascript.js
+++ javascript.js
@@ -25,14 +25,13 @@ function calculate() {
   let body = document.body;
   let calc = document.getElementById('calculadora');
   let titulo = document.getElementById('titulo');
-  let input = document.getElementById('theme-slider');
   let delet = document.getElementById('delete');
   let reset = document.getElementById('resete');
   let display = document.getElementById('display');
   let numero = document.querySelectorAll('.numero');
-  let igual = document.getElementsByClassName('.igual');
   let input2 = document.querySelectorAll('.input');
   let soma = document.getElementById('soma'); 
+  let positionTema = document.querySelectorAll('.positionTema');
   let backSlider = document.getElementById('backSlider');
   let slider1 = document.getElementById('slider1');
   let slider2 = document.getElementById('slider2');
@@ -58,130 +57,102 @@ function calculate() {
   }
 
 
-  // Aplica o tema selecionado
+  /////////////// PRIMEIRO TEMA///////////////
   if (range1.checked) {
-    slider1.classList.add('slider');
-    slider1.style.backgroundColor = '#C23B2D'; 
-    soma.classList.add('igual');
-    soma.style.backgroundColor = '#C23B2D';
-    soma.style.color = '#FFFFFF';
-    backSlider.classList.add('backSwitch');
-    backSlider.style.backgroundColor = '#181F32';
-    display.classList.add('result');
-    display.style.backgroundColor = '#181F32';
-    display.style.color = '#FFFFFF';
-    numero.forEach(function(elemento) {
-      elemento.classList.add('numero'); // Adiciona a classe nova
-      elemento.style.color = '#252D44';
-      elemento.style.backgroundColor = '#E5E4E0';
-    });
     body.classList.add('purpleMode');
     titulo.classList.add('tituloOne');
     calc.classList.add('calculadoraOne');
     delet.classList.add('deleteOne');
     reset.classList.add('resetOne'); 
-
+    backSlider.classList.add('backSwitchOne');
+    display.classList.add('resultOne');
+    soma.classList.add('igualOne');
+    slider1.classList.add('sliderOne');
+    numero.forEach(function(elemento) {
+      elemento.classList.add('numeroOne');
+      elemento.classList.remove('numeroTwo', 'numeroThree');
+    });
+    positionTema.forEach(function(value) {
+      value.classList.add('temaOne');
+      value.classList.remove('temaTwo', 'temaThree');
+    });
 
     //remover classes anteriores
-    slider2.classList.remove('slider');
-    slider3.classList.remove('slider');
-    slider3.style.backgroundColor = '';
-    slider2.style.backgroundColor = '';
-    range2.classList.remove('backSwitch');
-    range2.style.backgroundColor = '';
-    range3.classList.remove('backSwitch');
-    range3.style.backgroundColor = '';
-    
     body.classList.remove('darkMode', 'roxoMode');
     titulo.classList.remove('tituloTwo', 'tituloThree');
     delet.classList.remove('deleteTwo', 'deleteThree');
     reset.classList.remove('resetTwo', 'resetThree');
     calc.classList.remove('calculadoraTwo', 'calculadoraThree');
+    backSlider.classList.remove('backSwitchTwo', 'backSwitchThree');
+    display.classList.remove('resultTwo', 'resultThree');
+    soma.classList.remove('igualTwo', 'igualThree');
+    slider2.classList.remove('sliderTwo');
+    slider3.classList.remove('sliderThree');
     
-
+  /////////////// SEGUNDA TEMA///////////////
   } else if (range2.checked) {
-    slider2.classList.add('slider');
-    slider2.style.backgroundColor = '#FF8A38';
-    soma.classList.add('igual');
-    soma.style.backgroundColor = '#C23B2D';
-    soma.style.color = '#FFFFFF';
-    backSlider.classList.add('backSwitch');
-    backSlider.style.backgroundColor = '#D6CCCC';
-    display.classList.add('result');
-    display.style.backgroundColor = '#EEEEEE';
-    display.style.color = '#FFFFFF';
-    calc.classList.add('calculadora');
-    numero.forEach(function(elemento) {
-      elemento.classList.add('numero'); // Adiciona a classe nova
-      elemento.style.color = '#35352A';
-      elemento.style.backgroundColor = '#E5E4E0';
-    });
     body.classList.add('darkMode');
     titulo.classList.add('tituloTwo');
     calc.classList.add('calculadoraTwo');
     delet.classList.add('deleteTwo');
     reset.classList.add('resetTwo'); 
-
+    backSlider.classList.add('backSwitchTwo');
+    display.classList.add('resultTwo');
+    soma.classList.add('igualTwo');
+    slider2.classList.add('sliderTwo');
+    numero.forEach(function(elemento) {
+      elemento.classList.add('numeroTwo');
+      elemento.classList.remove('numeroOne', 'numeroThree');
+    });
+    positionTema.forEach(function(value) {
+      value.classList.add('temaTwo');
+      value.classList.remove('temaOne', 'temaThree');
+    });
  
     //remover classes anteriores
-    slider1.classList.remove('slider');
-    slider3.classList.remove('slider');
-    slider1.style.backgroundColor = '';
-    slider3.style.backgroundColor = '';
-    range3.classList.remove('backSwitch');
-    range3.style.backgroundColor = '';
-    range1.classList.remove('backSwitch');
-    range1.style.backgroundColor = '';
-
     body.classList.remove('purbleMode', 'roxoMode');
     titulo.classList.remove('tituloOne', 'tituloThree');
     delet.classList.remove('deleteOne', 'deleteThree');
     reset.classList.remove('resetOne', 'resetThree'); 
     calc.classList.remove('calculadoraOne', 'calculadoraThree'); 
+    backSlider.classList.remove('backSwitchOne', 'backSwitchThree');
+    display.classList.remove('resultOne', 'resultThree');
+    soma.classList.remove('igualOne', 'igualThree');
+    slider1.classList.remove('sliderOne');
+    slider3.classList.remove('sliderThree');
 
+  /////////////// TERCEIRO TEMA///////////////
   } else if (range3.checked) {
-    slider3.classList.add('slider');
-    slider3.style.backgroundColor = '#94FFF9';
-    soma.classList.add('igual');
-    soma.style.backgroundColor = '#94FFF9';
-    soma.style.color = '#331B4D';
-    backSlider.classList.add('backSwitch');
-    backSlider.style.backgroundColor = '#1E0836';
-    display.classList.add('result');
-    display.style.backgroundColor = '#1E0836';
-    display.style.color = '#FEE43B';
-    numero.forEach(function(elemento) {
-      elemento.classList.add('numero'); // Adiciona a classe nova
-      elemento.style.color = '#FEE43B';
-      elemento.style.backgroundColor = '#331B4D';
-    });
     body.classList.add('roxoMode');
     titulo.classList.add('tituloThree');
     calc.classList.add('calculadoraThree');
     delet.classList.add('deleteThree'); 
     reset.classList.add('resetThree'); 
-    
+    backSlider.classList.add('backSwitchThree');
+    display.classList.add('resultThree');
+    soma.classList.add('igualThree');
+    slider3.classList.add('sliderThree');
+    numero.forEach(function(elemento) {
+      elemento.classList.add('numeroThree');
+      elemento.classList.remove('numeroOne', 'numeroTwo');
+    });
+    positionTema.forEach(function(value) {
+      value.classList.add('temaThree');
+      value.classList.remove('temaOne', 'temaTwo');
+    });
 
     //remover classes anteriores
-    slider1.classList.remove('slider');
-    slider2.classList.remove('slider');
-    slider1.style.backgroundColor = '';
-    slider2.style.backgroundColor = '';
-    range1.classList.remove('backSwitch');
-    range1.style.backgroundColor = '';
-    range2.classList.remove('backSwitch');
-    range2.style.backgroundColor = '';
-
     body.classList.remove('purbleMode', 'darkMode');
     titulo.classList.remove('tituloTwo', 'tituloOne');
     delet.classList.remove('deleteOne', 'deleteTwo'); 
     reset.classList.remove('resetOne', 'resetTwo'); 
     calc.classList.remove('calculadoraOne', 'calculadoraTwo');
+    backSlider.classList.remove('backSwitchOne', 'backSwitchTwo');
+    display.classList.remove('resultOne', 'resultTwo');
+    soma.classList.remove('igualOne', 'igualTwo');
+    slider1.classList.remove('sliderOne');
+    slider2.classList.remove('sliderTwo');
 
-  } else {
-    
   }
-  
-
  }
  
