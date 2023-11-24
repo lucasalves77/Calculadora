fd5b20f622ddbeb727c821390a9678fc225f12ab
Lucas alves <lucasalvesdarocha007@gmail.com>
2023-11-24 10:46:26 -0300

    ultimo commit

--- javascript.js
+++ javascript.js
@@ -15,10 +15,10 @@ function calculate() {
   try {
       document.getElementById('display').value = eval(document.getElementById('display').value);
   } catch (error) {
-      document.getElementById('display').value = 'Error';
+      document.getElementById('display').value = '';
+     
   }
 }
-
 
 
  function trocarTema(temaSelecionado){
